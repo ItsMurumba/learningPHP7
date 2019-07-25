75a3f7fea9d87319dfd23b6bf14bd0f150bc85b7
KelvinWanyama <kevmurumba@gmail.com>
2019-07-25 08:46:50 +0300

    require and include options in PHP

--- index.php
+++ index.php
@@ -36,6 +36,11 @@ echo date("Y-m-d h:i:sa", $d) . "<br>";
 
 $d = strtotime("+3 Months");
 echo date("Y-m-d h:i:sa", $d) . "<br>";
+
+/**
+ * require: will produce a fatal error (E_COMPILE_ERROR) and stop the script. Used when the file is required by the application.
+ * include will only produce a warning (E_WARNING) and the script will continue. Used when the file is not required and application should continue when file is not found.
+ */
 ?>
 
 </body>
